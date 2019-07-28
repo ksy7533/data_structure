4cfd48bfcef8c23d3942be166de79ab51af93d87
ksy7533 <ksy7533@gmail.com>
2019-07-28 16:57:02 +0900

    list 데이터 구조 생성 및 테스트

--- list/list.js
+++ list/list.js
@@ -1,16 +1,20 @@
-var data = [];
-
 function List() {
   this.data = []; // 데이터 저장 형식
   this.pos = 0; // 현재 데이터 포지션값
   this.listSize = 0; // listSize
 }
 
+/*
+* list 뒤에 item 추가
+*/
 List.prototype.appendItem = function(item) {
-  data.push(item);
+  this.data.push(item);
   this.listSize++;
 }
 
+/*
+* 인자로 받은 item의 위치값 반환
+*/
 List.prototype.findItemPos = function(findItem) {
   let findIndex = -1;
   this.data.some((item, index) => {
@@ -22,41 +26,110 @@ List.prototype.findItemPos = function(findItem) {
   return findIndex;
 }
 
+/*
+* 첫번째 인자로 받은 item 뒤에 item 추가
+*/
 List.prototype.insertItem = function(beforeItem, insertItem) {
   const beforeIndexIndex = this.findItemPos(beforeItem);
-  this.data.splice(beforeIndexIndex, 1, insertItem);
+  if(!beforeIndexIndex === -1) return false;
+  this.data.splice(beforeIndexIndex, 0, insertItem);
   this.listSize++;
+  return true;
 }
 
+/*
+* 해당하는 item을 찾아서 삭제
+*/
 List.prototype.removeItem = function(item) {
-  const itemIndex = this.findItemPos(beforeItem);
+  const itemIndex = this.findItemPos(item);
+  if(!itemIndex === -1) return false;
   this.data.splice(itemIndex, 1);
   this.listSize--;
+  return true;
 }
 
-List.prototype.nextItem = function() {
-  if(this.listSize === pos) {
-    this.pos = 0;
-  } else {
-    this.pos++;
-  }
+/*
+* list에 해당하는 item이 있는지 확인
+*/
+List.prototype.contains = function(findItem) {
+  const findIndexNum = this.data.findIndex(item => item === findItem)
+  return findIndexNum > -1 ? true : false;
+}
+
+/*
+* list사이즈를 반환
+*/
+List.prototype.length = function() {
+  return this.listSize;
+}
+
+
+/*
+* list 초기화
+*/
+List.prototype.clear = function() {
+  delete this.data;
+  this.listSize = 0;
+  this.pos = 0;
+  this.data = [];
+}
+
+// 탐색 메소드
+
+/*
+* 첫번째 위치로 이동
+*/
+List.prototype.front = function() {
+  this.pos = 0;
+}
+
+/*
+* 마지막 위치로 이동
+*/
+List.prototype.end = function() {
+  this.pos = this.listSize - 1;
+}
+
+/*
+* 현재 pos의 다음 위치로 이동
+*/
+List.prototype.next = function() {
+  if(this.listSize === this.pos) return;
+  this.pos++;
+}
+
+/*
+* 현재 pos의 이전 위치로 이동
+*/
+List.prototype.prev = function() {
+  if(this.pos === -1) return;
+  this.pos--;
+}
+
+/*
+* 현재 pos의 위치값 반환
+*/
+List.prototype.currentPos = function() {
+  return this.pos;
+}
+
+/*
+* 해당 position으로 이동
+*/
+List.prototype.moveToPos = function(position) {
+  this.pos = position;
+}
+
+/*
+* 현재 item 반환
+*/
+List.prototype.currentItem = function() {
   return this.data[this.pos];
 }
 
-List.prototype.prevItem = function() {
-  if(this.listSize === 0) {
-    this.pos = this.listSize - 1;
-  } else {
-    this.pos--;
-  }
-  return this.data[this.pos];
+/*
+* list의 모든 요소 출력
+*/
+List.prototype.toString = function() {
+  return this.data;
 }
-
-List.prototype.getFirstItem = function() {
-  return this.data[0];
-}
-
-List.prototype.getLastItem = function() {
-  return this.data[listSize - 1];
-}
-
