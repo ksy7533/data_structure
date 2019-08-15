214467d4a6682b193650fc78d30719067e76fcba
ksy7533 <ksy7533@gmail.com>
2019-08-15 16:42:08 +0900

    순환형 연결리스트 생성, 조세퍼스 순열문제 순환형 연결리스트로 해결

--- linkedList/circleLinkedList.js
+++ linkedList/circleLinkedList.js
@@ -3,13 +3,14 @@ function Node(element) {
   this.next = null;
 }
 
-function LList() {
+function CLList() {
   this.head = new Node('head');
+  this.head.next = this.head;
   this.currentNode = this.head;
 }
 
 /* 노드 찾기 */
-LList.prototype.find = function(item) {
+CLList.prototype.find = function(item) {
   let currentNode = this.head;
   while(currentNode.element !== item) {
     currentNode = currentNode.next;
@@ -18,7 +19,7 @@ LList.prototype.find = function(item) {
 }
 
 /* node 끼워넣기 */
-LList.prototype.insert = function(newElement, item) {
+CLList.prototype.insert = function(newElement, item) {
   const beforeInsertItem = this.find(item);
   const newNode = new Node(newElement);
   newNode.next = beforeInsertItem.next;
@@ -26,16 +27,16 @@ LList.prototype.insert = function(newElement, item) {
 }
 
 /* linked list 순회 출력 */
-LList.prototype.display = function() {
+CLList.prototype.display = function() {
   let currentNode = this.head;
-  while(currentNode.next !== null) {
+  while(currentNode.next !== null && currentNode.next.element !== 'head') {
     console.log(currentNode.next.element);
     currentNode = currentNode.next;
   }
 }
 
 /* 이전 엘리멘트 찾기 */
-LList.prototype.findPrevious = function(item) {
+CLList.prototype.findPrevious = function(item) {
   let currentNode = this.head;
   while(currentNode.element !== null && currentNode.next.element !== item) {
     currentNode = currentNode.next;
@@ -44,7 +45,7 @@ LList.prototype.findPrevious = function(item) {
 }
 
 /* 엘리멘트 삭제 */
-LList.prototype.remove = function(item) {
+CLList.prototype.remove = function(item) {
   let findPrevNode = this.findPrevious(item);
   if(findPrevNode !== null) {
     findPrevNode.next = findPrevNode.next.next;
@@ -52,8 +53,12 @@ LList.prototype.remove = function(item) {
 }
 
 /* n노드만큼 전진 */
-LList.prototype.advance = function(n) {
+CLList.prototype.advance = function(n) {
   for(let i = 0; i < n; i++) {
     this.currentNode = this.currentNode.next;
+    if(this.currentNode === this.head) {
+      this.currentNode = this.currentNode.next;
+    }
   }
 }
+
--- linkedList/test.js
+++ linkedList/test.js
@@ -10,4 +10,36 @@ function test() {
   // debugger
   twice.remove('다현');
   twice.display();
+  console.log(twice.currentNode);
+  twice.advance(2);
+  console.log(twice.currentNode);
 };
+
+
+/*
+  조세퍼스 문제는 다음과 같다.
+
+  1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다.
+  이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다.
+  이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-조세퍼스 순열이라고 한다.
+
+  예를 들어 (7, 3)-조세퍼스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+
+  N과 K가 주어지면 (N, K)-조세퍼스 순열을 구하는 프로그램을 작성하시오.
+*/
+
+
+function josephusProblem(num, intervalNum) {
+  let result = []; // 출력 결과물
+  const circleList = new CLList();
+  for(let i = 1; i <= num; i++) {
+    circleList.insert(i, circleList.currentNode.element);
+    circleList.advance(1);
+  }
+  while(circleList.head.next !== circleList.head) {
+    circleList.advance(intervalNum);
+    result.push(circleList.currentNode.element);
+    circleList.remove(circleList.currentNode.element);
+  }
+  console.log(result)
+}
