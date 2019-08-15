f865990c843873e3298125a4e8b508f7f16a00ef
ksy7533 <ksy7533@gmail.com>
2019-08-15 17:53:01 +0900

    dictionary 활용 예제 테스트

--- dictionary/test.js
+++ dictionary/test.js
@@ -11,3 +11,20 @@ function test() {
   dic.clear();
   console.log(dic.count());
 }
+
+/*
+인자로 받은 문자열의 포함된 단어의 갯수가 각각 몇개인지를 출력하라
+*/
+function countWord(str) {
+  let tempArr = null;
+  const dic = new Dictionary();
+  tempArr = str.split(' ');
+  tempArr.forEach(word => {
+    if(!dic.find(word)) {
+      dic.add(word, 1);
+    } else {
+      dic.datastore[word] = dic.find(word) + 1
+    }
+  });
+  dic.showAll()
+}
